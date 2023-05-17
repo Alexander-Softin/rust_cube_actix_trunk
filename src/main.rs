4e2e069863f360d85bf7f22b1d5abb87ec597c07
champashan <71319975+champashan@users.noreply.github.com>
2023-05-17 21:59:59 +0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,22 @@
-use bevy::prelude::*;
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(rotator_system)
-        .run();
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body(include_str!("dist/index.html"))
 }
+use actix_files as fs;
 
-/// this component indicates what entities should rotate
-#[derive(Component)]
-struct Rotator;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
 
-/// rotates the parent, which will result in the child also rotating
-fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
-    for mut transform in &mut query {
-        transform.rotate_x(3.0 * time.delta_seconds());
-    }
+
+    HttpServer::new(|| 
+        App::new()
+            .route("/", web::get().to(index))
+            .service(fs::Files::new("/static", "./").show_files_listing())
+
+    )
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run()
+    .await
 }
-
-/// set up a simple scene with a "parent" cube and a "child" cube
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 2.0 }));
-    let cube_material_handle = materials.add(StandardMaterial {
-        base_color: Color::rgb(165.0, 42.0, 42.0),
-        ..default()
-    });
-
-    // parent cube
-    commands
-        .spawn((
-            PbrBundle {
-                mesh: cube_handle.clone(),
-                material: cube_material_handle.clone(),
-                transform: Transform::from_xyz(0.0, 0.0, 1.0),
-                ..default()
-            },
-            Rotator,
-        ));
-        
-    // light
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 5.0, -4.0),
-        ..default()
-    });
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(5.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-} 
