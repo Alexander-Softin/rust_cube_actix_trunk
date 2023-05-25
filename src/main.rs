669a1c54d8ef52d8a97886f9268a11f857a3286a
champashan <71319975+champashan@users.noreply.github.com>
2023-05-25 20:45:29 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use mysql::prelude::*;
+use mysql::{self, OptsBuilder};
 
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body(include_str!("dist/index.html"))
+    HttpResponse::Ok().body(include_str!("index.html"));
+     // Connect to the MySQL database
+     let mut conn = mysql::Conn::new(
+        OptsBuilder::new()
+            .user(Some("root"))
+            .pass(Some("19992000Cc."))
+            .db_name(Some("mydatabase")),)
+
+    .unwrap();
+ 
+ // Execute a simple query
+    let rows = conn.query_iter("SELECT * FROM mydatabase.users WHERE id = 1").unwrap();
+    let result = rows
+         .map(|row| row.unwrap())
+         .collect::<Vec<_>>()
+         .pop()
+         .unwrap();
+
+    HttpResponse::Ok().body(format!("Result: {:?}", result))
 }
-use actix_files as fs;
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-
-    HttpServer::new(|| 
-        App::new()
-            .route("/", web::get().to(index))
-            .service(fs::Files::new("/static", "./").show_files_listing())
-
-    )
-    .bind("127.0.0.1:8080")
-    .unwrap()
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
